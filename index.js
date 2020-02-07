11f306c9b0952d28568c54d546e19a2bad0ebf80
aakansha <aakanshaarya2@gmail.com>
2020-02-07 13:22:22 +0530

    set up static files and layouts

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts')
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+//use express router
+app.use('/', require('./routes'));    //a middleware
+
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err) {
     if(err) {
@@ -9,3 +22,4 @@ app.listen(port, function(err) {
 
     console.log(`Server is running on port: ${port}`);
 });
+
